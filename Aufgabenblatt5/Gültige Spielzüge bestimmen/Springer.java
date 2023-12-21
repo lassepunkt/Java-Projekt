3d764936d058244e04f1e9cf22bfdc9f17b7a71d
lassepunkt <122087541+lassepunkt@users.noreply.github.com>
2023-12-21 20:15:28 +0100

    Update Springer.java

--- Aufgabenblatt5/Gültige Spielzüge bestimmen/Springer.java
+++ Aufgabenblatt5/Gültige Spielzüge bestimmen/Springer.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Springer implements Spielfigur {
     private Position position;
     private boolean black;
@@ -8,7 +10,15 @@ public class Springer implements Spielfigur {
     }
     @Override
     public ArrayList<Position> getMoves() {
-        
+        ArrayList<Position> list = new ArrayList<Position>();
+        for (int x = 0; x < 8; x++) {
+            for (int y = 0; y < 8; y++) {
+                if (this.checkValidMove(new Position(x,y))) {
+                    list.add(new Position(x,y));
+                }
+            }
+        }
+        return list;
     }
     @Override
     public Position getPosition() {
@@ -20,7 +30,17 @@ public class Springer implements Spielfigur {
     }
     @Override
     public boolean checkValidMove(Position position) {
-        
+        if (position.getX()<0 || position.getX()>7 || position.getY()<0 || position.getY()>7) {
+            return false;
+        }
+        boolean validMuster = Math.pow(Math.abs(position.getX()-this.position.getX()),2)+(Math.pow(Math.abs(position.getY()-this.position.getY()),2)) == 5;
+        boolean freiesFeld = false;
+        if (Main.spielfeld[position.getX()][position.getY()] == null) {
+            freiesFeld = true;
+        } else {
+            freiesFeld = this.black != Main.spielfeld[position.getX()][position.getY()].isBlack();
+        }
+    return validMuster && freiesFeld;
     }
     @Override
     public boolean isBlack() {

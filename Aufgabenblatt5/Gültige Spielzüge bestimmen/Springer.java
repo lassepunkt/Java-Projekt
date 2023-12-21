1df1bc452f86167c68a75f1d447aaa1a2a75cf34
lassepunkt <122087541+lassepunkt@users.noreply.github.com>
2023-12-21 19:32:53 +0100

    Create Springer.java

---
+++ Aufgabenblatt5/Gültige Spielzüge bestimmen/Springer.java
@@ -0,0 +1,9 @@
+public class Springer implements Spielfigur {
+    private Position position;
+    private boolean black;
+    
+    public Springer(Position position, boolean black) {
+        this.position = position;
+        this.black = black;
+    }
+}
